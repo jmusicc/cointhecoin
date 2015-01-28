bb883c8db160750e98c186b9d32e6a8cba20f3c4
jmusicc <jjanikowski92@gmail.com>
2015-01-28 11:02:12 -0600

    Updated loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,5 @@
 var cSite, 
-    sList = ["primedice.com", "fortunejack.com", "coinichiwa.com", "bitdice.me"];
+    sList = ["primedice.com", "fortunejack.com", "prcdice.eu", "bikinidice.com", "coinichiwa.com", "bitdice.me", "safedice.com", "satoshidice.com", "win88.me", "777coin.com"];
 
 // Get Site
 getSite = function () {
@@ -11,7 +11,20 @@ getSite = function () {
     }
 };
 getSite();
-   
-var boturl = '//cdn.rawgit.com/jmusicc/cointhecoin/master/src/' +cSite+ '.js';
-var script = '<script src="' + boturl + '"></script>';
-$(script).appendTo('head');
+function inject() {
+ 	var boturl = '//cdn.rawgit.com/jmusicc/cointhecoin/master/src/' +cSite+ '.js';
+	var script = '<script src="' + boturl + '"></script>';
+	$(script).appendTo('head');
+} 
+// check if site has jQuery
+if (cSite == sList[1] || sList[3] || sList[5]) {
+	// Inject jQuery using JS syntax
+  	var e = document.createElement('script');
+	e.setAttribute('src', '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js');
+	document.body.appendChild(e);
+  setTimeout(function(){
+  	inject();
+  }, 1000);
+} else {
+	inject();	  
+}
